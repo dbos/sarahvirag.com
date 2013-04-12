6a333ca6cc36aca4445f67022ae121818dab83de
Matthew Poirier <matthew@computing-power.(none)>
2013-04-12 15:01:52 -0400

    Updates, still not templated

--- httpdocs/scripts.js
+++ httpdocs/scripts.js
@@ -2,7 +2,18 @@
 */
 
 jQuery(function($) {
-  $('aside').draggable({containment:'parent'});
+  $(window).load(function () {
+    return false;
+    $('#slideshow ul').show();
+    $('aside').html('<h2></h2>').removeClass('loading');
+    slideImage(0,0);
+/*
+    $('#slideshow ul li img').addpowerzoom({
+      defaultpower : 3,
+      powerrange : [5,5],
+      magnifiersize : [250,250] 
+    });
+*/ 
   $(document)
     .on('mousewheel keydown', function (e, d) {
     if (!$('#slideshow ul').queue().length) {
@@ -30,7 +41,17 @@ jQuery(function($) {
       } 
     }
     }
+  }).on('click', '#legend ul li a', function (e) {
+    e.preventDefault();
+    var clickedIndex = $(this).parent().index();
+    if (!$('#slideshow ul').queue().length) {
+      console.log('clickedIndex',clickedIndex);
+      slideImage(0,clickedIndex);
+    }
   });
+  });
+
+  $('aside').draggable({containment:'parent'});
 
   var slideDuration = 1000;
  
@@ -40,10 +61,12 @@ jQuery(function($) {
     $aside.find('h2').text(title).fadeIn(); 
   }
  
-  function slideImage_unfiltered (dir) {
+  function slideImage_unfiltered (dir,target) {
     var $images = $('#slideshow li');
     var total = $images.length - 1;
-    var cur = $('#slideshow .active').removeClass('active').index() + dir;
+    var curStart = $('#slideshow .active').removeClass('active').index();
+    var cur = target === 0 ? 0 : (target || curStart + dir);
+    // var cur = target != oldCur ? target : false || cur + dir;
     var curHeight = $(window).height();
     
     if (cur>total) {
@@ -55,6 +78,7 @@ jQuery(function($) {
     }
    
     $('aside *').fadeOut(); 
+    $('#legend ul').animate({'margin-top':-(cur==total?0:cur)*30}, slideDuration*.75);
     $('#slideshow ul').animate({'margin-top':-cur*curHeight}, slideDuration, 'easeInOutCirc', function () {
       if (cur <= 0) {
         cur = total;
@@ -63,6 +87,7 @@ jQuery(function($) {
       }
       $images.eq(cur).addClass('active').parent().css('margin-top', -cur*curHeight);
       updateAside($('.image-wrap.active'));
+      console.log('cur', cur);
     });
   }
   var slideImage = _.throttle(slideImage_unfiltered, slideDuration, true);
