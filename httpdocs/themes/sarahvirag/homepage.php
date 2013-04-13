ccd16cc11b7662d40198c4e6d45bb5b13d1ac879
Ubuntu <ubuntu@ip-10-249-43-70.us-west-2.compute.internal>
2013-04-13 01:13:48 +0000

    brought back images

--- httpdocs/themes/sarahvirag/homepage.php
+++ httpdocs/themes/sarahvirag/homepage.php
@@ -14,7 +14,7 @@ $this->inc('inc/top.php'); ?>
         --><li class="image-wrap"><img src="img/pic2.DELETEME.jpg" alt="" title="Falling flowers"></li><!--
         --><li class="image-wrap"><img src="img/pic3.DELETEME.jpg" alt="" title="Wow, look at that fucking boat"></li><!--
         --><li class="image-wrap"><img src="http://nightwing2303.com/wp-content/uploads/2011/11/First-Impression-adidas-Crazy-8-1.jpg" alt="" title="God damn, these are some sneakers."></li><!--
-        --><li class="image-wrap"><img src="http://konachan.com/sample/374f6cc096bfbea0f2b128eca4e83f5a/Konchan.com%20-%2043672%20sample.jpg" alt="" title="Sup ladies."></li><!--
+        --><li class="image-wrap"><img src="img/pic4.DELETEME.jpg" alt="" title="Sup ladies."></li><!--
         --><li class="image-wrap endpiece"><img src="img/pic1.DELETEME.jpg" title="Some buildings by a river"></li>
         </ul>
     </section>
